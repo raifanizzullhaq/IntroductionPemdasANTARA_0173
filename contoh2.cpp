8216e483fd42edecc7b4f8a18cdcabe38fe0035b
raifanizzullhaq <raifanizzulhaq3>
2026-07-09 20:51:57 +0700

    menambahkan program utama

--- contoh2.cpp
+++ contoh2.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{//Mulai
+    //Numeric nPanjang, nLebar, nKeliling
+    int Panjang, Lebar, Keliling;
+
+    //Display "Masukkan Panjang : "
+    cout << "Masukkan Panjang : ";
+    //Accept nPanjang
+    cin >> Panjang;
+    //Display "Masukkan Lebar : "
+    cout << "Masukkan Lebar : ";
+    //Accept nLebar
+    cin >> Lebar;
+
+    //Compute nKeliling = 2(p+l)
+    Keliling = 2 * (Panjang + Lebar);
+
+    //Display "Keliling Persegi Panjang : " + nKeliling
+    cout << "Keliling Persegi Panjang : " << Keliling;
+
+}//Selesai
